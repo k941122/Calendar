daf6fd7457741ad8ff86a4e45069cc41aba506f7
k941122 <941122k@naver.com>
2023-11-09 00:26:11 +0900

    네이버 api axios를 이용해 제작하였으나 api 엑세스 토큰을 받지 못함

--- src/calendar_componet.js
+++ src/calendar_componet.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState,userEffect } from "react";
 import Calendar from "react-calendar";
 import moment from "moment/moment";
 import "./calendar.css";
+import axios from "axios";
 
 const Calendarcomponent = ({ onChange, value }) => {
   const [nowDate, setNowDate] = useState("날짜");
+  const [events, setEvents] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const accessToken = ''//네이버 엑세스 토큰 받기
+      try {
+        const reseponse = await axios.get('', {headers:{'Authorization':'Bearer ${accessToken}',}});//네이버 캘린더 api
+      } catch (e) {
+        setError(e);
+      }
+    };
+    setEvents();//네이버 api 구조에 맞게 변경
+  })
 
   const handleDateChange = (selectedDate) => {
     onChange(selectedDate);
     setNowDate(moment(selectedDate).format("YYYY년 MM월 DD일"));
-    console.log("테스트");
+    //선택한 날짜에 맞는 이벤트를 fetch 하기
   };
   return (
     <div className="CalendarContainer">
@@ -20,6 +35,7 @@ const Calendarcomponent = ({ onChange, value }) => {
           value={value}
           formatDay={(locale, date) => moment(date).format("DD")}
           showNeighboringMonth={false}
+          //이벤트를 표시하는 로직 표기할수 있음
         />
       </div>
     </div>
