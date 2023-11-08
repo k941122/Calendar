846bb4286e762176b506d5621936a5af43d555a6
k941122 <941122k@naver.com>
2023-11-09 03:45:21 +0900

    App.js파일 오류 수정 및 todo.js 파일 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calendarcomponent from './calendar_componet';
+import Todolist from './todo';
 
 function App() {
   return (
     <div className="App">
      <Calendarcomponent onChange={(date) => {console.log(date);}} value = {new Date()} />
+     <Todolist/>
     </div>
   );
 }
